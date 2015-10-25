c2512db2cdd34e3007d9abc9b7ae91919f8c9804
Tiago <tiago.asp.net@gmail.com>
2015-10-25 14:22:05 -0200

    I created a contact list that returns json in http://localhost:3000/contatos

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -1,2 +1,9 @@
-var model = require('../models/contato.js');
+module.exports = function(app) {
+	var model = app.models.contato;
+	var controller = {};
+	controller.listaContatos = function(req, res){
+		res.json(model.listaContatos());
+	};
+	return controller;
+};
 
---
+++ app/models/contato.js
@@ -0,0 +1,11 @@
+module.exports = function(){	
+	var model = {};
+	model.listaContatos = function(){
+		var contatos = [];
+		for(var i=1; i<=5; i++){
+			contatos.push({_id: i, nome: 'Contato Exemplo '+i,email: 'cont'+i+'@empresa.com.br'});
+		};
+		return contatos;
+	};
+	return model;
+};
