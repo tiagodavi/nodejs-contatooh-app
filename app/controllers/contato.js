1ec299adb7523aae63728808689f0a5645298c5a
Tiago <tiago.asp.net@gmail.com>
2015-10-27 16:56:39 -0200

    I added the resource contatos/:id that fetches a contact of list

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -4,6 +4,13 @@ module.exports = function(app) {
 	controller.listaContatos = function(req, res){
 		res.json(model.listaContatos());
 	};
+	controller.obtemContato = function(req, res){
+		var idContato = req.params.id;
+		var contato = model.listaContatos().filter(function(contato){			
+			return contato._id == idContato;
+		});
+		contato ? res.json(contato) : res.status(404).send('Contato não encontrato');		
+	};
 	return controller;
 };
 
