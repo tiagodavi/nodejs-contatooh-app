c86b67617a9cbd3f92f1a440486df85c685b162f
Tiago <tiago.asp.net@gmail.com>
2015-11-03 16:03:50 -0200

    Build a list of all contacts
    Build a list of all contacts with ng-repeat and then filter it.
    Add bootstrap for improving the appearance.
    Implement two way data binding with ng-model.

--- public/js/controllers/ContactsController.js
+++ public/js/controllers/ContactsController.js
@@ -4,5 +4,24 @@ angular.module('contatooh').controller('ContactsController',
 		$scope.increment = function(){
 			$scope.total++;
 		};
+		$scope.filter = '';
+		$scope.contacts = 
+		[
+			{
+				"_id": 1,
+				"name": "Fabio",
+				"email": "oi@empresa.com.br"
+			},
+			{
+				"_id": 2,
+				"name": "Jose",
+				"email": "embratel@empresa.com.br"
+			},
+			{
+				"_id": 3,
+				"name": "Fernanda",
+				"email": "tim@empresa.com.br"
+			}
+		];
 	}
 );
