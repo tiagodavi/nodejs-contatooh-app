0b3a354570caa95380d268a0e7dcb6ba5e9b462c
Tiago <tiago.asp.net@gmail.com>
2015-11-19 10:18:30 -0200

    Add custom messages for users

--- public/js/controllers/ContactsController.js
+++ public/js/controllers/ContactsController.js
@@ -2,16 +2,26 @@ angular.module('contatooh').controller('ContactsController',
 	function($scope, $resource){		
 		$scope.filter 	 = '';
 		$scope.contacts  = [];	
+		$scope.message 	 = {text: ''}
 
 		var Contact = $resource('/contatos/:id');
+		var Message = {			
+			set: function(text){
+				$scope.message.text = text;
+			},
+			clean: function(){
+				$scope.message.text = '';
+			}
+		};
 		var fetchContatcs = function(){
 			Contact.query(
 				function(contacts){				
 					$scope.contacts = contacts;
+					Message.clean();
 				},
 				function(error){
-					console.log("Contacts not found");
-					console.log(error);
+					Message.set('Contacts not found');
+					console.log(error);												
 				}
 			);
 		};
@@ -20,7 +30,7 @@ angular.module('contatooh').controller('ContactsController',
 			Contact.delete({id: contact._id},
 				fetchContatcs,
 				function(error){
-					console.log('Error in removing the contact');
+					Message.set('Error in removing the contact');
 					console.log(error);
 				}
 			);
