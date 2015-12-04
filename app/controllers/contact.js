3e9abf085426fbb905c53cf7638dad5e235c7464
Tiago <tiago.asp.net@gmail.com>
2015-12-04 11:09:28 -0200

    Change all to EcmaScript 6 and use Mongoose

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -1,49 +1,70 @@
-module.exports = function(app) {
-	var model 		= app.models.contact;
-	var controller 	= {};
-	var contacts 	= {};
-	var contact_ID 	= 5;
-	
-	var insert	= function(contact){
-		contact._id = ++contact_ID;
-		contacts.push(contact);
-		return contact;
-	};
-	var update = function(contact){
-		contacts = contacts.map(function(_contact){
-			if(_contact._id == contact._id){
-				_contact = contact;
+module.exports = app => {
+	var Model 		= app.models.contact
+	var controller 	= {}
+		
+	controller.findAll = (req, res) => {
+		Model.find().exec()
+		.then(
+			contacts => res.json(contacts),
+			error => { 
+				console.log(error)
+				res.status(500).json(error)
+			} 			
+		)
+	}	
+	controller.findOne = (req, res) => {
+		var _id = req.params.id
+			
+		Model.findById(_id).exec()
+		.then(
+			contact => {
+				if (! contact) throw new Error("Contact not found");
+				res.json(contact)
+			},
+			error => {
+				console.log(error)
+				res.status(404).json(error)
 			}
-			return _contact;
-		});
-		return contact;		
-	};
+		)		
+	}
+	controller.remove = (req, res) => {
+		var _id = req.params.id
 
-	controller.findAll = function(req, res){
-		res.json(contacts);
-	};
-	controller.findOne = function(req, res){
-		var contactId = req.params.id;
-		var contact = contacts.filter(function(contact){			
-			return contact._id == contactId;
-		});
-		contact[0] ? res.json(contact[0]) : res.status(404).send('contact not found');		
-	};
-	controller.remove = function(req, res){
-		var contactId = req.params.id;
-		contacts = contacts.filter(function(contact){			
-			return contact._id != contactId;
-		});
-		res.send(204).end();
-	};
-	controller.save = function(req, res) {
-		var contact = req.body;		
-		contact = contact._id ?
-			insert(contact) :
-			update(contact);
-		res.json(contact);
-	};
+		Model.remove({"_id": _id}).exec()
+		.then(
+			() => res.end(),
+			error => {
+				console.log(error)
+				res.status(500).json(error)
+			}
+		)		
+	}
+	controller.save = (req, res) =>  {
+		var inputContact = req.body
 
-	return controller;
-};
+		if(inputContact._id){
+			Model.findByIdAndUpdate(inputContact._id, inputContact)
+			.exec()
+			.then(
+				contact => res.json(contact),
+				error => {
+					console.log(error)
+					res.status(500).json(error)
+				}
+			)
+		}
+		else{
+			Model.create(inputContact)
+			.then(
+				contact => res.status(201).json(contact),
+				error => {
+					console.log(error)
+					res.status(500).json(error)
+				}
+			)
+		}	
+	}
+
+	return controller
+}
 
